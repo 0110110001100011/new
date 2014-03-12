ef26a6e444ace4790db1b8df0cb2d6dd39f7106c
Luis Carlos <lctc91@gmail.com>
2014-03-12 01:23:22 -0600

    base

---
+++ MvcApplication1/MvcApplication1/Controllers/HomeController.vb
@@ -0,0 +1,21 @@
+﻿Public Class HomeController
+    Inherits System.Web.Mvc.Controller
+
+    Function Index() As ActionResult
+        ViewData("Message") = "bla bla  this template to jump-start your ASP.NET MVC application."
+
+        Return View()
+    End Function
+
+    Function About() As ActionResult
+        ViewData("Message") = "HEXABYTES!!!."
+
+        Return View()
+    End Function
+
+    Function Contact() As ActionResult
+        ViewData("Message") = "Contacta al equipo Hexabytes"
+
+        Return View()
+    End Function
+End Class
